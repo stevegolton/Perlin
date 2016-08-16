4deae66953b690a32e5c143954235da5cdb25933
Steve Golton <stevegolton@gmail.com>
2016-08-16 23:47:15 +0100

    Proper perlin noise using gradient vectors and cosine interpolation.

--- main.py
+++ main.py
@@ -3,42 +3,68 @@ import random # Pseudo random number library
 import numpy
 import math
 
-featuresize = 10
+featuresize = 20
 
-def lerp(val,lower,upper):
-	res = lower*(1-val) + upper*val
-	return res
+# val should be in the range 0.0 - 1.0
+def lerp( lower, upper, val ):
+	return lower*(1-val) + upper*val
 
-def lerp2d(x,y,ul,ur,ll,lr):
-	x1 = lerp( x, ul, ur )
-	x2 = lerp( x, ll, lr )
-	return lerp( y, x1, x2 )
+def cosinterp( y1, y2, mu ):
+	mu2 = (1-math.cos(mu*math.pi))/2;
+	return(y1*(1-mu2)+y2*mu2)
+
+def dotGridGradient(ix, iy, x, y):
+	# Compute the distance vector
+	dx = x - float(ix);
+	dy = y - float(iy);
+ 
+	# Compute the dot-product
+	return dx*gradient[iy,ix,0] + dy*gradient[iy,ix,1]
 
 def perlin(x,y):
 	
-	xfrac = float(x)/featuresize
-	yfrac = float(y)/featuresize
-
 	# Find our closest feature node
-	x0 = math.floor(x/featuresize)
-	y0 = math.floor(y/featuresize)
+	x0 = math.floor(x)
+	y0 = math.floor(y)
+
+	if ( x <= 0.0 ):
+		x0 -= 1
+	if ( y <= 0.0 ):
+		y0 -= 1
 
 	x1 = x0 + 1
 	y1 = y0 + 1
 
-	#val = int(lerp( xfrac - x0, table[x0,y0], table[x1,y0]))
-	val = int( lerp2d( xfrac - x0, yfrac - y0, table[x0,y0], table[x1,y0], table[x0,y1], table[x1,y1] ) )
-	return ( val, val, val );
+	# Interpolation weights
+	sx = x - x0
+	sy = y - y0
+
+	n0 = dotGridGradient(x0, y0, x, y);
+	n1 = dotGridGradient(x1, y0, x, y);
+	ix0 = cosinterp(n0, n1, sx);
+
+	n0 = dotGridGradient(x0, y1, x, y);
+	n1 = dotGridGradient(x1, y1, x, y);
+	ix1 = cosinterp(n0, n1, sx);
+
+	value = cosinterp(ix0, ix1, sy)
+	pval = int( 128 * ( value + 1.0 ) )
+	
+	return ( pval, pval, pval );
 
 # Generate a random map
-table = numpy.empty( shape=(1000,1000) )
+gradient = numpy.empty( shape=(200,200,2) )
 
-for i in range(1000):    # for every pixel:
-    for j in range(1000):
-        table[i,j] = random.randint( 0, 255 )
+for i in range(200):    # for every node:
+    for j in range(200):
+		x = random.uniform(-1.0, 1.0)
+		y = random.uniform(-1.0, 1.0)
+		m = math.sqrt(x*x + y*y)
+		gradient[i,j,0] = x / m
+		gradient[i,j,1] = y / m
 
 # Create a new image 255x255px and fill it with black pixels initially
-img = Image.new( 'RGB', (255, 255), "black")
+img = Image.new( 'RGB', (500, 500), "black")
 
 # Gets the pixelmap from the image
 pixels = img.load()
@@ -46,7 +72,7 @@ pixels = img.load()
 # Fill in pixels with a pretty colour
 for j in range(img.size[1]):
 	for i in range(img.size[0]):    # for every pixel:
-		pixels[i,j] = perlin( i, j )
+		pixels[i,j] = perlin( float(i)/featuresize, float(j)/featuresize )
 
 img.show()
 
